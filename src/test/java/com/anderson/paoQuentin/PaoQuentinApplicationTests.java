24864597b464b87156927c5a7ef4b82f28d973ac
Anderson-Batista <andersonbfs15@gmail.com>
2021-08-20 09:17:17 -0300

    Commit inicial

---
+++ src/test/java/com/anderson/paoQuentin/PaoQuentinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.paoQuentin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaoQuentinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
