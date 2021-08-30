10f2d770a712244afb940f8157c530403b21e6e0
Anderson-Batista <andersonbfs15@gmail.com>
2021-08-30 15:57:26 -0300

    DELETE para Fornada

--- src/main/java/com/anderson/paoQuentin/service/FornadaService.java
+++ src/main/java/com/anderson/paoQuentin/service/FornadaService.java
@@ -39,4 +39,9 @@ public class FornadaService {
 		obj.setStatus(objDto.getStatus());
 		return repository.save(obj);
 	}
+
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
+	}
 }
